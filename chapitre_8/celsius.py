438dba3e0b647e77085a734a2b9ba9f5e6ab3249
chuck2kill <jlardeux@yahoo.fr>
2020-06-22 12:15:21 +0200

    celsius.1.0.1

--- chapitre_8/celsius.py
+++ chapitre_8/celsius.py
@@ -9,22 +9,31 @@ vice versa"""
 from tkinter import *
 
 # définitions de fonctions
-def convertFa(event):
-    fahrenheit.select_clear()
-    f = int(celsius.get())
-    f = f * 22
-    fahrenheit.insert(0, f)
+def convertFa(event):                       # conversion des celsius en fahrenheit
+    fahrenheit.delete(0, END)               # on commence par effacer le contenu de la cellule
+    f = (int(celsius.get()) * (9 / 5)) + 32 # on fait le calcul
+    fahrenheit.insert(0, f)                 # et on l'affiche
+
+def convertCe(event):                       # conversion de fahrenheit en celsius
+    celsius.delete(0, END)                  # meme fonctionnement mais à l'inverse
+    c = (int(fahrenheit.get()) - 32) * (5 / 9)
+    celsius.insert(0, c)
 
 # ---------- Programme principal ----------
 fen = Tk()
+fen.title('Conversion Celsius/Fahrenheit')
+# Création des libellés
 Label(fen, text='température en degrés Celsius').grid(row=0, column=0)
 Label(fen, text='Température en degrés Fahrenheit').grid(row=1, column=0)
+# Création des zones de textes
 celsius = Entry(fen, fg='red')
 celsius.grid(row=0, column=1)
-celsius.bind("<Return>", convertFa)
+celsius.bind("<Return>", convertFa)         # en appuyant sur entrée, on appelle la fonction
 fahrenheit = Entry(fen, fg='green')
 fahrenheit.grid(row=1, column=1)
-
+fahrenheit.bind("<Return>", convertCe)      # en appuyant sur entrée, on appelle la fonction
+# Création du bouton quitter
 Button(fen, text='Quitter', command=fen.quit).grid(row=2, column=1, sticky=E)
 
+# Activation de la boucle d'événements
 fen.mainloop()
