6386fc876d1fa4cd7bafeb92d7f4e8a67398c7a1
chuck2kill <jlardeux@yahoo.fr>
2020-07-01 18:38:35 +0200

    rebond.1.0

--- chapitre_8/rebond.py
+++ chapitre_8/rebond.py
@@ -8,12 +8,29 @@ la gravité"""
 from tkinter import *
 
 # définition de fonction
-def rebond():
-    global x, y, yi, step
-    y = y + step * 2
+def move():
+    global x, y, yh, v, dx, flag
+    yh += 20
+    v += dx                     # augmentation de la vitesse v
+    y += v                      # on ajoute la vitesse v au point y
+    if y > 400:
+        y = 400
+        v = -v
+    
     can.coords(balle, x - 20, y - 20, x + 20, y + 20)
-    if y < yi:
-        fen.after(50, rebond)
+    if flag > 0:
+        fen.after(50, move)
+
+def start():
+    global flag
+    flag += 1
+    if flag == 1:
+        move()
+
+def stop():
+    global flag
+    flag = 0
+
 
 #========== Programme principal ==========
 
@@ -22,12 +39,13 @@ fen = Tk()
 fen.title('Balle rebondissante')
 
 # variables globales
-x, y, yi = 250, 100, 450
-step = 10
+x, y, yh, v, dx, flag = 250, 100, 100, 0, 6, 0
 
 # création du canevas
 can = Canvas(fen, width=500, height=500, bg='light blue')
-can.grid(row=0, column=0, rowspan=2)
+canBouton = Canvas(fen)
+canBouton.grid(row=0, column=1, sticky=N)
+can.grid(row=0, column=0, rowspan=3)
 
 # création des graphiques
 sol = can.create_rectangle(0, 400, 500, 500, fill='light green', width=0)
@@ -35,8 +53,9 @@ soleil = can.create_oval(400, 20, 450, 70, fill='yellow', width=0)
 balle = can.create_oval(x - 20, y - 20, x + 20, y + 20, fill='red', width=2)
 
 # création des boutons
-Button(fen, text='Quitter', command=fen.quit, width=10).grid(row=1, column=1, sticky=S)
-Button(fen, text='Démarrage', command=rebond, width=10).grid(row=0, column=1, sticky=N)
+Button(fen, text='Quitter', command=fen.quit, width=10).grid(row=2, column=1, sticky=S)
+Button(canBouton, text='Démarrage', command=start, width=10).grid(row=0, column=0, sticky=N)
+Button(canBouton, text='Arrêt', command=stop, width=10).grid(row=1, column=0, sticky=N)
 
 # démarrage du réceptionnaire d'événements (boucle principale) :
 fen.mainloop()
