0168ce86f196f3f2401eea12fa7938762aedbf11
chuck2kill <jlardeux@yahoo.fr>
2020-07-03 15:54:21 +0200

    jeu_balle1.0.5

--- chapitre_8/jeu_balle1.py
+++ chapitre_8/jeu_balle1.py
@@ -12,28 +12,26 @@ from random import *
 
 # définition des fonctions
 def move():
-    global x, y, xMax, yMax, flag, dx, dy
+    global x, y, xMax, yMax, flag, dx, dy, v
     y += dy
     x += dx
 
     if y > 480:
         y = 480
-        dy = randrange(1, 10)
-        dy = -dy
+        dy = randrange(-10, -1)
     elif y < 20:
         y = 20
         dy = randrange(1, 10)
     if x > 480:
         x = 480
-        dx = randrange(1, 10)
-        dx = -dx
+        dx = randrange(-10, -1)
     elif x < 20:
         x = 20
         dx = randrange(1, 10)   
     
     can1.coords(balle, x - 20, y - 20, x + 20, y + 20)
     if flag == 1:
-        fen.after(10, move)
+        fen.after(v, move)
 
 def start():
     global flag
@@ -45,6 +43,17 @@ def stop():
     global flag
     flag = 0
 
+def compte(event):
+    global clic, score, v
+    clic += 1
+    if event.x < x + 20 and event.x > x - 20 and event.y < y + 20 and event.y > y - 20:
+        score += 1
+        v -= 15
+    if clic >= 10:
+        stop()
+        scoreF = Label(fen, text='score = ' + str(score), width=50)
+        scoreF.grid(row=0, column=0)
+
 #========== Programme prinscipal ==========
 fen = Tk()
 fen.title('Clique la balle 1.0')
@@ -53,22 +62,25 @@ fen.title('Clique la balle 1.0')
 xMax, yMax = 500, 500
 x, y = xMax / 2, yMax / 2
 dx, dy = randrange(1, 10), randrange(1, 10)
+v = 100
 flag = 0
+clic, score = 0, 0
 
 # création des canevas
-can1 = Canvas(fen, width=xMax, height=yMax, bg='orange')
+can1 = Canvas(fen, width=xMax, height=yMax, bg='blue')
 can1.grid(row=0, column=0, rowspan=2)
 can2 = Canvas(fen)
 can2.grid(row=0, column=1, sticky=N)
 
 # Affichage de la balle
-balle = can1.create_oval(x - 20, y - 20, x + 20, y + 20, fill='blue', width=1)
+balle = can1.create_oval(x - 20, y - 20, x + 20, y + 20, fill='orange', width=1)
 
 # création des boutons
 Button(can2, text='Démarrer', command=start, width=10).grid(row=0, column=0, padx=10, pady=5)
 Button(can2, text='Arrêter', command=stop, width=10).grid(row=1, column=0, padx=10, pady=5)
 Button(fen, text='Quitter', command=fen.quit, width=10).grid(row=1, column=1, padx=10, pady=10, sticky=S)
 
+can1.bind('<Button-1>', compte)
  
 # démarrage du réceptionnaire d'événements (boucle principale) :
 fen.mainloop()
