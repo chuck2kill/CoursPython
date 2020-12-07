d44b0bd08df6ccccf9daceee94b55fee577d3d57
chuck2kill <57481234+chuck2kill@users.noreply.github.com>
2020-12-07 21:28:55 +0100

    serpent V2.0
    version définitive

--- chapitre_8/serpent.py
+++ chapitre_8/serpent.py
@@ -24,30 +24,34 @@ def new_ball():
 def go_left(event =None):
     "Déplacement vers la gauche"
     global dx, dy, sens
-    dx, dy = -1, 0
-    sens = 0
+    if sens != 2:
+        dx, dy = -1, 0
+        sens = 0
 
 def go_right(event =None):
     "Déplacement vers la droite"
     global dx, dy, sens
-    dx, dy = 1, 0
-    sens = 2
+    if sens != 0:
+        dx, dy = 1, 0
+        sens = 2
 
 def go_up(event =None):
     "Déplacement vers le haut"
     global dx, dy, sens
-    dx, dy = 0, -1
-    sens = 1
+    if sens != 3:
+        dx, dy = 0, -1
+        sens = 1
 
 def go_down(event =None):
     "Déplacement vers le bas"
     global dx, dy, sens
-    dx, dy = 0, 1
-    sens = 3
+    if sens != 1:
+        dx, dy = 0, 1
+        sens = 3
 
 def move():
     "Animation du serpent par récursivité"
-    global flag, serp, y, x
+    global flag, serp, y, x, score
     # Principe du mouvement opéré : on déplace le carré de queue, dont les
     # caractéristiques sont mémorisées dans le premier élément de la liste
     # <serp>, de manière à l'amener en avant du carré de tête, dont les
@@ -67,7 +71,7 @@ def move():
     # Vérification : a-t-on atteint les limites du canvas ? :
     if xq < 0 or xq > canX - cc or yq < 0 or yq > canY - cc:
         flag = 0                # => arrêt de l'animation
-        can.create_text(canX / 2, 20, anchor=CENTER, text="Perdu !!!",
+        can.create_text(canX / 2, 20, anchor=CENTER, text="Perdu !!! Score: " + str(score),
                         fill="red", font="Arial 14 bold")
     can.coords(cq, xq, yq, xq + cc, yq + cc)    # Déplacement effectif
     serp.append([cq, xq, yq])                   # Mémorisation du nouveau carré de tête
@@ -75,36 +79,38 @@ def move():
 
     # vérification que le serpent ne se coupe pas lui même
     i = 0
-    while i < len(serp):
+    while i < len(serp) - 1:
         h = serp[i][1]
         j = serp[i][2]
         if serp[len(serp) - 1][1] in range(h, h + cc) and serp[len(serp) - 1][2] in range(j, j + cc):
             flag = 0                # => arrêt de l'animation
-            can.create_text(canX / 2, 20, anchor=CENTER, text="Perdu !!!",
+            can.create_text(canX / 2, 20, anchor=CENTER, text="Perdu !!! Score: " + str(score),
                         fill="red", font="Arial 14 bold")
+        i += 1
 
     # Gestion de la collision avec la balle
     if (can.bbox(balle)[0] in range(serp[len(serp) - 1][1], serp[len(serp) - 1][1] + 15) or can.bbox(balle)[2] in range(serp[len(serp) - 1][1], serp[len(serp) - 1][1] + 15)) and (can.bbox(balle)[1] in range(serp[len(serp) - 1][2], serp[len(serp) - 1][2] + 15) or can.bbox(balle)[3] in range(serp[len(serp) - 1][2], serp[len(serp) - 1][2] + 15)):
-            can.delete(balle)                   # suppression de l'ancienne balle
-            new_ball()                          # création d'une nouvelle balle
-            x = serp[0][1]                      # on stocke les infos du dernier carré du serpent
-            y = serp[0][2]
-            if sens == 0:                       # si se déplace vers la gauche
-                x = x + cc
-                carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
-                serp.insert(0, [carre, x, y])
-            elif sens == 2:                     # si se déplace vers la droite
-                x = x - cc
-                carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
-                serp.insert(0, [carre, x, y])
-            elif sens == 1:                     # si se déplace vers le haut
-                y = y + cc
-                carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
-                serp.insert(0, [carre, x, y])
-            else:                               # si se déplace vers le bas
-                y = y - cc
-                carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
-                serp.insert(0, [carre, x, y])
+        can.delete(balle)                   # suppression de l'ancienne balle
+        new_ball()                          # création d'une nouvelle balle
+        x = serp[0][1]                      # on stocke les infos du dernier carré du serpent
+        y = serp[0][2]
+        if sens == 0:                       # si se déplace vers la gauche
+            x = x + cc
+            carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
+            serp.insert(0, [carre, x, y])
+        elif sens == 2:                     # si se déplace vers la droite
+            x = x - cc
+            carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
+            serp.insert(0, [carre, x, y])
+        elif sens == 1:                     # si se déplace vers le haut
+            y = y + cc
+            carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
+            serp.insert(0, [carre, x, y])
+        else:                               # si se déplace vers le bas
+            y = y - cc
+            carre = can.create_rectangle(x , y, x + cc, y + cc, fill="green")
+            serp.insert(0, [carre, x, y])
+        score += 1
 
     # Appel récursif de la fonction par elle-même (=> boucle d'animation)
     if flag > 0:
@@ -117,6 +123,7 @@ def move():
 flag = 0                            # commutateur pour l'animation
 dx, dy = 1, 0                       # indicateurs pour le sens de déplacement
 sens = 2                            # variable qui détermine le sens de déplacement
+score = 0                           # variable de score
 
 # Autres variables globales :
 canX, canY = 500, 500                                                       # dimensions du canvas
